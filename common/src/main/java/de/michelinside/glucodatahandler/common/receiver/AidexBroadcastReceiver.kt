c7316c15ad253f5e55849d84f506cc83d834855b
Riley Gall <rileyg98@gmail.com>
2026-02-03 09:03:45 +1000

    Add try/catch to capture

--- common/src/main/java/de/michelinside/glucodatahandler/common/receiver/AidexBroadcastReceiver.kt
+++ common/src/main/java/de/michelinside/glucodatahandler/common/receiver/AidexBroadcastReceiver.kt
@@ -18,26 +18,31 @@ class AidexBroadcastReceiver : NamedBroadcastReceiver() {
 
     override fun onReceiveData(context: Context, intent: Intent) {
         Log.d(LOG_ID, "onReceiveData called for action ${intent.action}")
-        if (intent.action == "com.microtechmd.cgms.NOTIFICATION") {
-            val message = intent.getSerializableExtra("message")
-            if (message is BleMessage) {
-                if (message.operation == 1 && message.isSuccess) {
-                    val rawData = message.data
-                    val serialNumber = intent.getStringExtra("sn")
-                    val record = AiDexCgmParser.parse(rawData, serialNumber)
-                    if (record != null) {
-                        Log.d(LOG_ID, "Parsed record: $record")
-                        val extras = Bundle()
-                        extras.putLong(ReceiveData.TIME, record.timestamp)
-                        extras.putFloat(ReceiveData.GLUCOSECUSTOM, record.glucose)
-                        extras.putInt(ReceiveData.MGDL, GlucoDataUtils.mmolToMg(record.glucose).toInt())
-                        extras.putString(ReceiveData.SERIAL, record.serialNumber)
-                        // Battery is not directly supported in ReceiveData bundle, but we have it.
-                        // extras.putInt("battery", record.battery)
-                        ReceiveData.handleIntent(context, DataSource.AIDEX, extras)
+        try {
+            if (intent.action == "com.microtechmd.cgms.NOTIFICATION") {
+                val message = intent.getSerializableExtra("message")
+                if (message is BleMessage) {
+                    if (message.operation == 1 && message.isSuccess) {
+                        val rawData = message.data
+                        val serialNumber = intent.getStringExtra("sn")
+                        val record = AiDexCgmParser.parse(rawData, serialNumber)
+                        if (record != null) {
+                            Log.d(LOG_ID, "Parsed record: $record")
+                            val extras = Bundle()
+                            extras.putLong(ReceiveData.TIME, record.timestamp)
+                            extras.putFloat(ReceiveData.GLUCOSECUSTOM, record.glucose)
+                            // Convert the native mmol/L back into mg/dL for GDH
+                            extras.putInt(ReceiveData.MGDL, GlucoDataUtils.mmolToMg(record.glucose).toInt())
+                            extras.putString(ReceiveData.SERIAL, record.serialNumber)
+                            // Battery is not directly supported in ReceiveData bundle, but we have it.
+                            // extras.putInt("battery", record.battery)
+                            ReceiveData.handleIntent(context, DataSource.AIDEX, extras)
+                        }
                     }
                 }
             }
+        } catch (exc: Exception) {
+            Log.e(LOG_ID, "onReceiveData exception: " + exc.message.toString() )
         }
     }
 }
